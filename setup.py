1626a8db9926e14ae0086ffdf778199911c42b3d
Juan A. Rubio <jarubio2001@gmail.com>
2015-11-13 13:38:47 +0000

    Updated copyrights (a bit late)

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 
 # Copyright (C) 2011-2014 Aratelia Limited - Juan A. Rubio
 #
-# Copyright (C) 2010, 2011 Linaro
-#
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
